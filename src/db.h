558f4cfb6d941e355107145e554acb7f5270d38f
Oliver Smith <osmith@sysmocom.de>
2019-03-06 13:17:39 +0100

    db_hlr.c: db_subscr_create(): nam_cs, nam_ps args
    Allow creating new subscribers without giving them access to CS or PS.
    This will be used by the create-subscriber-on-demand feature.
    
    Related: OS#2542
    Change-Id: I1a6dd85387723dab5487c53b33d2d9ec6d05d006

--- src/db.h
+++ src/db.h
@@ -118,7 +118,7 @@ struct sub_auth_data_str {
 	} u;
 };
 
-int db_subscr_create(struct db_context *dbc, const char *imsi);
+int db_subscr_create(struct db_context *dbc, const char *imsi, int nam_cs, int nam_ps);
 int db_subscr_delete_by_id(struct db_context *dbc, int64_t subscr_id);
 
 int db_subscr_update_msisdn_by_imsi(struct db_context *dbc, const char *imsi,
